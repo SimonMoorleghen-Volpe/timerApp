2a5ec6759706b2c892714224af70f965d7aa22a9
Simon Moorleghen-Volpe <wscvolpe@gmail.com>
2023-06-17 10:14:20 -0400

    working timer component

--- src/App.vue
+++ src/App.vue
@@ -3,71 +3,6 @@
     
     import timerComp from './timerComp.vue'
 
-    const timers = ref([])
-    const timer_name = ref('')
-    const input_hours = ref(0)
-    const input_minutes = ref(0)
-    const input_seconds = ref(0)
-    var id = 0
-
-    
-
-    const addTimer = () => {
-        if(input_hours.value === 0 && input_minutes.value === 0 && input_seconds.value === 0){ return }
-
-        var timerComponent = createApp(timerComp, {inpTime:input_hours.value * 3600 + input_minutes.value * 60 + input_seconds.value,
-                                                    inpName: timer_name.value})
-        
-        var el = document.createElement('div')
-        document.getElementById('timerDisplay').appendChild(el)
-        timerComponent.mount(el)
-
-        // if(timers.value.length > 0){ return }
-        // var newNode = createElementVNode(timerComp)
-        // newNode.time = input_hours.value * 3600 + input_minutes.value * 60 + input_seconds.value
-        // newNode.name = timer_name.value
-        // timers.value.push( {
-        //     name: timer_name.value,
-        //     time: input_hours.value * 3600 + input_minutes.value * 60 + input_seconds.value,
-        //     ID: id
-        // })
-        // id += 1
-
-    }
-
-
-    watch(input_hours, (newHour) => {
-        if(typeof(newHour) == "string"){
-            input_hours.value = 0
-        }
-        if(newHour > 23){
-            input_hours.value = 23
-        } else if(newHour < 0){
-            input_hours.value = 0
-        }
-    })
-
-    watch(input_minutes, (newMinute) => {
-        if(typeof(newMinute) == "string"){
-            input_minutes.value = 0
-        }
-        if(newMinute > 59){
-            input_minutes.value = 59
-        } else if(newMinute < 0){
-            input_minutes.value = 0
-        }
-    })
-
-    watch(input_seconds, (newSecond) => {
-        if(typeof(newSecond) == "string"){
-            input_seconds.value = 0
-        }
-        if(newSecond > 59){
-            input_seconds.value = 59
-        } else if(newSecond < 0){
-            input_seconds.value = 0
-        }
-    })
 
 </script>
 
@@ -76,50 +11,8 @@
             <h1>Personal Timers</h1>
         </section>
     <main class="app">
-        
-        <section class="timer_block">
-            <section class="create_timer">
-                <form @submit.prevent="addTimer">  
+        <timerComp></timerComp>
 
-                    <h3>What is the timer for?</h3>
-
-                    <input type="text" 
-                    placeholder="e.g. work on project"
-                    v-model="timer_name" />
-                    <h3> Enter the duration.</h3>
-                    <div class="time_select">
-                        <input 
-                            type="number" 
-                            placeholder="00" 
-                            v-model="input_hours">
-                        <h4>h&nbsp;</h4>
-                        <input 
-                            type="number" 
-                            placeholder="00" 
-                            v-model="input_minutes">
-                        <h4>m&nbsp;</h4>
-                        <input 
-                            type="number" 
-                            placeholder="00" 
-                            v-model="input_seconds">
-                        <h4>s</h4>
-                    </div>
-                  
-                    <input type="submit" value="Add Timer" />
-                </form>
-
-            </section>
-        </section>
-        <section class="timer_list" id="timerDisplay">
-            <h3>Active Timer</h3>
-
-            
-
-            
-
-
-        </section>
-    
     </main>
     
 
--- src/timerComp.vue
+++ src/timerComp.vue
@@ -1,89 +1,101 @@
-<template>
-
-    <input type="text" v-model="timerName" readonly="readonly" />
-
-    <div>
-        <h4>{{ hour }} : {{ minute }} : {{ second }}</h4>
-    </div>   
-
-</template>
-
-<script>
-    import {h, ref, watch} from 'vue'
-    export default {
-        props:{
-            inpTime: Number,
-            inpName: String
-        },
-        setup(props){
 
 
+<script setup>
+    import { computed } from '@vue/reactivity';
+    import {h, readonly, ref, watch} from 'vue'
+    
 
-        },
-        data: function () {
-            return {
-                time: ref(this.inpTime),
-                timerName: ref(this.inpName),
-                hour: ref(Math.floor(this.inpTime/3600)),
-                minute: ref(Math.floor(this.inpTime/60) % 60),
-                second: ref(this.inpTime % 60),
-                run
-            }   
-        },
-        mounted () {
-            run = setInterval(function () {this.decrementTime()}, 1000)
-            console.log(run)
-        },
-        // watch: {
-        //     hour(val)  {
-        //         this.$el.queryselector('#hour').innerText = val
-        //     },
+    const inputHours = ref(0)
+    const inputMinutes = ref(0)
+    const inputSeconds = ref(0)
 
-        //     minute(val) {
-        //         this.$el.queryselector('#minute').innerText = val
-        //     },
-
-        //     second(val){
-        //         this.$el.queryselector('#second').innerText = val
-        //     }
-        // },
-        methods: {
-            decrementTime() {
-                time.value -= 1
-                hour.value = Math.floor(time.value/3600)
-                minute.value = Math.floor(time.value/60) % 60
-                second.value = time.value % 60
-
-                console.log(hour.value, minute.value, second.value, time.value)
-                
-                if(time.value <= 0){
-                    clearInterval(run)
-                }
-            }
-        }
-    }
-
-    var timerName = ref('')
+    const hour = ref(null)
+    const minute = ref(null)
+    const second = ref(null)
 
     const time = ref(0)
+    var run
 
-    const hour = ref(0)
-    const minute = ref(0)
-    const second = ref(0)
-    let run = null
+    watch((inputHours), (newHours)=>{
+        inputHours.value = remove_alpha_characters(newHours)
+        if(inputHours.value.length > 2){
+            inputHours.value = inputHours.value.slice(1)
+        }
+        if(Number(inputHours.value) >= 24){
+            inputHours.value = '23'
+        }
+    })
+    watch((inputMinutes), (newMinutes)=>{
+        inputMinutes.value = remove_alpha_characters(newMinutes)
+        if(inputMinutes.value.length > 2){
+            inputMinutes.value = inputMinutes.value.slice(1)
+        }
+        if(Number(inputMinutes.value) >= 60){
+            inputMinutes.value = '59'
+        }
+    })
+    watch((inputSeconds), (newSeconds)=>{
+        inputSeconds.value = remove_alpha_characters(newSeconds)
+        if(inputSeconds.value.length > 2){
+            inputSeconds.value = inputSeconds.value.slice(1)
+        }
+        if(Number(inputSeconds.value) >= 60){
+            inputSeconds.value = '59'
+        }
+    })
 
-    let decrementTime = () => {
+    const start = () => {
+        console.log("Start called")
+        time.value = 3600 * Number(inputHours.value) + 60 * Number(inputMinutes.value) + Number(inputSeconds.value)
+
+        hour.value.setAttribute( "readonly", true)
+        minute.value.setAttribute( "readonly", true)
+        second.value.setAttribute( "readonly", true)
+
+        console.log(hour.value, minute.value, second.value)
+
+        run = setInterval(function () {decrementTime()}, 1000)
+    }
+
+    const decrementTime = () => {
+        console.log('a')
         time.value -= 1
-        hour.value = Math.floor(time.value/3600)
-        minute.value = Math.floor(time.value/60) % 60
-        second.value = time.value % 60
+        inputHours.value = (Math.floor(time.value / 3600)).toString()
+        inputMinutes.value = (Math.floor(time.value / 60) % 60).toString()
+        inputSeconds.value = (time.value % 60).toString()
         if(time.value <= 0){
             clearInterval(run)
+            run = null
+            hour.value.removeAttribute( "readonly")
+            minute.value.removeAttribute( "readonly")
+            second.value.removeAttribute( "readonly")
+            console.log(hour.value, minute.value, second.value)
         }
     }
 
-    
-    
-    
+
+    const remove_alpha_characters = (inString) => {
+        return inString.replace(/\D/g, '')
+    }
 
 </script>
+
+<template>
+
+    <div class="timerComp">
+        <div>
+            <input type="text" id="hour" ref="hour" v-model="inputHours">
+            <label for="hour" class="timerLabel"> h  </label>
+            <input type="text" id="minute" ref="minute" v-model="inputMinutes">
+            <label for="minute" class="timerLabel"> m  </label>
+            <input type="text" id="second" ref="second" v-model="inputSeconds">
+            <label for="second" class="timerLabel"> s</label>
+        </div>
+        
+
+        <button @click="start">Start</button>
+
+    </div>
+    
+
+</template>
